dd3426d633aede317fb68c5063aabfdac5bc1f28
Akash Vaidya <akash.vaidya23@gmail.com>
2025-04-13 19:15:18 +0530

    Add user profile and history routes with password change and avatar upload support

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { registerUser, loginUser, logoutUser, refreshAccessToken } = require("../controllers/user.controller.js");
+const { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } = require("../controllers/user.controller.js");
 const router = Router();
 const upload = require("../middlewares/multer.middleware.js");
 const  verifyJwt = require("../middlewares/auth.middleware.js");
@@ -24,5 +24,11 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changePassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router.route("/coverImage").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.route("/channel-history/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/watch-history").get(verifyJwt, getWatchHistory);
 
 module.exports = router;
